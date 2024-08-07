2cb2e34c38dd6c44825c7025eefd42f19b8b1e26
indesperate <indesperate@outlook.com>
2024-08-07 22:18:19 +0800

    finish job get finish

--- hw-map-reduce-rs/src/coordinator/mod.rs
+++ hw-map-reduce-rs/src/coordinator/mod.rs
@@ -5,7 +5,6 @@ use std::collections::{HashMap, VecDeque};
 use std::sync::Arc;
 
 use anyhow::Result;
-use bytes::Bytes;
 use tokio::sync::Mutex;
 use tokio::time::Instant;
 use tonic::transport::Server;
@@ -14,7 +13,7 @@ use tonic::{Request, Response, Status};
 
 use crate::app::named;
 use crate::rpc::coordinator::*;
-use crate::{JobId, WorkerId, COORDINATOR_ADDR, INITIAL_JOB_ID, INITIAL_WORKER_ID};
+use crate::{JobId, TaskNumber, WorkerId, COORDINATOR_ADDR, INITIAL_JOB_ID, INITIAL_WORKER_ID};
 
 pub mod args;
 
@@ -33,14 +32,112 @@ pub struct WorkerState {
     last_alive_time: Instant,
 }
 
+#[derive(Debug, Clone)]
+pub enum TaskState {
+    Idle,
+    Running,
+    Done,
+}
+
+#[derive(Debug, Clone)]
+pub struct MapTask {
+    id: TaskNumber,
+    file: String,
+    state: TaskState,
+}
+
+#[derive(Debug, Clone)]
+pub struct ReduceTask {
+    id: TaskNumber,
+    state: TaskState,
+}
+
+#[derive(Debug)]
+pub enum JobProcess {
+    Map,
+    Reduce,
+    Running,
+    Done,
+}
+
 pub struct JobState {
-    files: Vec<String>,
+    map_tasks: Vec<MapTask>,
+    reduce_tasks: Vec<ReduceTask>,
+    map_workers: Vec<MapTaskAssignment>,
     output_dir: String,
     app: String,
     n_reduce: u32,
-    args: Bytes,
-    done: bool,
-    failed: bool,
+    args: Vec<u8>,
+    job_process: JobProcess,
+}
+
+impl JobState {
+    pub fn new(job: SubmitJobRequest) -> Self {
+        let mut reduce_tasks = Vec::new();
+        for id in 0..job.n_reduce {
+            reduce_tasks.push(ReduceTask {
+                id: id.try_into().unwrap(),
+                state: TaskState::Idle,
+            })
+        }
+        JobState {
+            map_tasks: job
+                .files
+                .into_iter()
+                .enumerate()
+                .map(|(id, file)| MapTask {
+                    id,
+                    file,
+                    state: TaskState::Idle,
+                })
+                .collect(),
+            reduce_tasks,
+            map_workers: Vec::new(),
+            output_dir: job.output_dir,
+            app: job.app,
+            n_reduce: job.n_reduce,
+            args: job.args,
+            job_process: JobProcess::Map,
+        }
+    }
+
+    pub fn get_reduce_job(&mut self) -> Option<&ReduceTask> {
+        let task = self
+            .reduce_tasks
+            .iter_mut()
+            .find(|t| matches!(t.state, TaskState::Idle))?;
+        task.state = TaskState::Running;
+        Some(task)
+    }
+
+    pub fn get_map_job(&mut self) -> Option<&MapTask> {
+        let task = self
+            .map_tasks
+            .iter_mut()
+            .find(|t| matches!(t.state, TaskState::Idle))?;
+        task.state = TaskState::Running;
+        Some(task)
+    }
+
+    pub fn check_reduce_is_running(&mut self) {
+        if self
+            .reduce_tasks
+            .iter()
+            .all(|t| matches!(t.state, TaskState::Done | TaskState::Running))
+        {
+            self.job_process = JobProcess::Running;
+        }
+    }
+
+    pub fn check_map_is_running(&mut self) {
+        if self
+            .map_tasks
+            .iter()
+            .all(|t| matches!(t.state, TaskState::Done | TaskState::Running))
+        {
+            self.job_process = JobProcess::Running;
+        }
+    }
 }
 
 pub struct Coordinator {
@@ -93,18 +190,7 @@ impl coordinator_server::Coordinator for Coordinator {
         let job = req.into_inner();
         named(&job.app).map_err(|e| Status::new(Code::InvalidArgument, e.to_string()))?;
         // add to queue and map
-        inner.jobs.insert(
-            job_id,
-            JobState {
-                files: job.files,
-                output_dir: job.output_dir,
-                app: job.app,
-                n_reduce: job.n_reduce,
-                args: job.args.into(),
-                done: false,
-                failed: false,
-            },
-        );
+        inner.jobs.insert(job_id, JobState::new(job));
         inner.job_queue.push_back(job_id);
 
         log::info!("Receive job {}", job_id);
@@ -125,8 +211,8 @@ impl coordinator_server::Coordinator for Coordinator {
         };
 
         Ok(Response::new(PollJobReply {
-            done: job_state.done,
-            failed: job_state.failed,
+            done: matches!(job_state.job_process, JobProcess::Done),
+            failed: false,
             errors: Vec::new(),
         }))
     }
@@ -165,8 +251,8 @@ impl coordinator_server::Coordinator for Coordinator {
         &self,
         req: Request<GetTaskRequest>,
     ) -> Result<Response<GetTaskReply>, Status> {
-        // TODO: Tasks
-        Ok(Response::new(GetTaskReply {
+        let mut inner = self.inner.lock().await;
+        let mut task = GetTaskReply {
             job_id: 0,
             output_dir: "".to_string(),
             app: "".to_string(),
@@ -178,14 +264,100 @@ impl coordinator_server::Coordinator for Coordinator {
             wait: true,
             map_task_assignments: Vec::new(),
             args: Vec::new(),
-        }))
+        };
+        // return idle task
+        let job_id = match inner.job_queue.iter().find(|id| {
+            matches!(
+                inner.jobs.get(id).expect("Job not in job map").job_process,
+                JobProcess::Map | JobProcess::Reduce
+            )
+        }) {
+            Some(&j) => j,
+            None => return Ok(Response::new(task)),
+        };
+        task.job_id = job_id;
+        let job_state = inner.jobs.get_mut(&job_id).expect("Job not in job map!!!");
+        task.output_dir = job_state.output_dir.clone();
+        task.app = job_state.app.clone();
+        task.n_reduce = job_state.n_reduce;
+        task.n_map = job_state.map_tasks.len().try_into().unwrap();
+        task.args = job_state.args.clone();
+        let worker_id = req.into_inner().worker_id;
+        match job_state.job_process {
+            JobProcess::Map => {
+                if let Some(map_task) = job_state.get_map_job() {
+                    log::info!("Send Map job to worker {}!!", worker_id);
+                    task.task = map_task.id.try_into().unwrap();
+                    task.file = map_task.file.clone();
+                    task.reduce = false;
+                    task.wait = false;
+                    job_state.map_workers.push(MapTaskAssignment {
+                        worker_id,
+                        task: task.task,
+                    });
+                }
+                job_state.check_map_is_running();
+            }
+            JobProcess::Reduce => {
+                if let Some(reduce_task) = job_state.get_reduce_job() {
+                    log::info!("Send reduce job to worker {} !!", worker_id);
+                    task.task = reduce_task.id.try_into().unwrap();
+                    task.reduce = true;
+                    task.wait = false;
+                    task.map_task_assignments = job_state.map_workers.clone();
+                }
+                job_state.check_reduce_is_running();
+            }
+            _ => {}
+        };
+        Ok(Response::new(task))
     }
 
     async fn finish_task(
         &self,
         req: Request<FinishTaskRequest>,
     ) -> Result<Response<FinishTaskReply>, Status> {
-        // TODO: Tasks
+        let task_req = req.into_inner();
+        let mut inner = self.inner.lock().await;
+        let job_state = inner
+            .jobs
+            .get_mut(&task_req.job_id)
+            .expect("Job not in job map!!!");
+        let task_id: usize = task_req.task.try_into().unwrap();
+        log::info!(
+            "Job done with {} {}, reduce ? {}",
+            task_req.worker_id,
+            task_req.task,
+            task_req.reduce
+        );
+        if task_req.reduce {
+            let reduce_task = job_state
+                .reduce_tasks
+                .get_mut(task_id)
+                .expect("Task not in reduce task");
+            reduce_task.state = TaskState::Done;
+            if job_state
+                .reduce_tasks
+                .iter()
+                .all(|t| matches!(t.state, TaskState::Done))
+            {
+                job_state.job_process = JobProcess::Done;
+                inner.job_queue.pop_front();
+            }
+        } else {
+            let map_task = job_state
+                .map_tasks
+                .get_mut(task_id)
+                .expect("Task not in map task!!");
+            map_task.state = TaskState::Done;
+            if job_state
+                .map_tasks
+                .iter()
+                .all(|t| matches!(t.state, TaskState::Done))
+            {
+                job_state.job_process = JobProcess::Reduce;
+            }
+        }
         Ok(Response::new(FinishTaskReply {}))
     }
 
