0f4f67a25ff26a7e3cc8bb49ce98bf72145ed823
indesperate <indesperate@outlook.com>
2024-08-08 00:47:05 +0800

    finish fault

--- hw-map-reduce-rs/src/coordinator/mod.rs
+++ hw-map-reduce-rs/src/coordinator/mod.rs
@@ -12,8 +12,11 @@ use tonic::Code;
 use tonic::{Request, Response, Status};
 
 use crate::app::named;
-use crate::rpc::coordinator::*;
-use crate::{JobId, TaskNumber, WorkerId, COORDINATOR_ADDR, INITIAL_JOB_ID, INITIAL_WORKER_ID};
+use crate::rpc::{coordinator::*, worker};
+use crate::{
+    JobId, TaskNumber, WorkerId, COORDINATOR_ADDR, INITIAL_JOB_ID, INITIAL_WORKER_ID,
+    TASK_TIMEOUT_SECS,
+};
 
 pub mod args;
 
@@ -26,10 +29,19 @@ pub struct CoordinatorState {
     job_queue: VecDeque<JobId>,
 }
 
+#[derive(Debug, Clone)]
+pub struct WorkerTask {
+    job: JobId,
+    task: TaskNumber,
+    reduce: bool,
+}
+
 /// a type for track worker state
+#[derive(Debug, Clone)]
 pub struct WorkerState {
     worker_id: WorkerId,
     last_alive_time: Instant,
+    tasks: Vec<WorkerTask>,
 }
 
 #[derive(Debug, Clone)]
@@ -156,6 +168,60 @@ impl Coordinator {
             })),
         }
     }
+
+    async fn check_workers(&self) {
+        let mut inner = self.inner.lock().await;
+        for crash_worker in Coordinator::get_crash_workers(&mut inner.workers).into_iter() {
+            for task in crash_worker.tasks.into_iter() {
+                let job: u32 = task.job;
+                let job_state = inner.jobs.get_mut(&job).expect("Job not in job map!!!");
+                if task.reduce {
+                    let reduce_task = job_state
+                        .reduce_tasks
+                        .get_mut(task.task)
+                        .expect("Task not in reduce task");
+                    if let TaskState::Running = reduce_task.state {
+                        reduce_task.state = TaskState::Idle;
+                        job_state.job_process = JobProcess::Reduce;
+                        job_state
+                            .map_workers
+                            .retain(|map| map.worker_id != crash_worker.worker_id)
+                    }
+                } else {
+                    let map_task = job_state
+                        .map_tasks
+                        .get_mut(task.task)
+                        .expect("Task not in map task");
+                    map_task.state = TaskState::Idle;
+                    job_state.job_process = JobProcess::Map;
+                    job_state
+                        .map_workers
+                        .retain(|map| map.worker_id != crash_worker.worker_id);
+                    for reduce_task in job_state.reduce_tasks.iter_mut() {
+                        if let TaskState::Running = reduce_task.state {
+                            reduce_task.state = TaskState::Idle;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn get_crash_workers(workers: &mut Vec<WorkerState>) -> Vec<WorkerState> {
+        let mut crash_workers = Vec::new();
+        let now = Instant::now();
+        workers.retain(|worker| {
+            let time_out = now - worker.last_alive_time;
+            if time_out.as_secs() >= TASK_TIMEOUT_SECS {
+                crash_workers.push(worker.clone());
+                log::info!("crash worker detected {}", worker.worker_id);
+                false
+            } else {
+                true
+            }
+        });
+        crash_workers
+    }
 }
 
 impl Default for Coordinator {
@@ -242,6 +308,7 @@ impl coordinator_server::Coordinator for Coordinator {
         inner.workers.push(WorkerState {
             worker_id,
             last_alive_time: Instant::now(),
+            tasks: Vec::new(),
         });
         log::info!("Worker register with {}", worker_id);
         Ok(Response::new(RegisterReply { worker_id }))
@@ -251,6 +318,7 @@ impl coordinator_server::Coordinator for Coordinator {
         &self,
         req: Request<GetTaskRequest>,
     ) -> Result<Response<GetTaskReply>, Status> {
+        self.check_workers().await;
         let mut inner = self.inner.lock().await;
         let mut task = GetTaskReply {
             job_id: 0,
@@ -286,8 +354,8 @@ impl coordinator_server::Coordinator for Coordinator {
         match job_state.job_process {
             JobProcess::Map => {
                 if let Some(map_task) = job_state.get_map_job() {
-                    log::info!("Send Map job to worker {}!!", worker_id);
                     task.task = map_task.id.try_into().unwrap();
+                    log::info!("Send Map job {} to worker {}!!", task.task, worker_id);
                     task.file = map_task.file.clone();
                     task.reduce = false;
                     task.wait = false;
@@ -300,7 +368,7 @@ impl coordinator_server::Coordinator for Coordinator {
             }
             JobProcess::Reduce => {
                 if let Some(reduce_task) = job_state.get_reduce_job() {
-                    log::info!("Send reduce job to worker {} !!", worker_id);
+                    log::info!("Send reduce job {} to worker {} !!", task.task, worker_id);
                     task.task = reduce_task.id.try_into().unwrap();
                     task.reduce = true;
                     task.wait = false;
@@ -308,8 +376,19 @@ impl coordinator_server::Coordinator for Coordinator {
                 }
                 job_state.check_reduce_is_running();
             }
-            _ => {}
+            _ => return Ok(Response::new(task)),
         };
+        // set worker status
+        let worker_state = inner
+            .workers
+            .iter_mut()
+            .find(|worker| worker.worker_id == worker_id)
+            .expect("Worker not in worker state");
+        worker_state.tasks.push(WorkerTask {
+            job: job_id,
+            task: task.task.try_into().unwrap(),
+            reduce: task.reduce,
+        });
         Ok(Response::new(task))
     }
 
